6c7e6c98efbd91d5fb6ca5947727d8f5ceb9af00
Deko-Dev <deiser@gmail.com>
2021-01-02 16:14:18 -0500

    Estilo del Navbar y Home terminado

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'contactos', component: ContactsComponent },
 ];
 
 @NgModule({
