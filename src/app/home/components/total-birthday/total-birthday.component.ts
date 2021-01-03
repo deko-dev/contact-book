a57f79bbbb3d0ebbd78264fb17b9fa72bf74f67a
Deko-Dev <deiser@gmail.com>
2021-01-02 22:37:16 -0500

    Corrigiendo Error en Home

--- src/app/home/components/total-birthday/total-birthday.component.ts
+++ src/app/home/components/total-birthday/total-birthday.component.ts
@@ -12,24 +12,30 @@ export class TotalBirthdayComponent implements OnInit {
 
   constructor() { 
     let arrayContactos: Contacto[] = JSON.parse(localStorage.getItem('contactos'));
+    
+    if(arrayContactos.length > 0){
+      
+      let arrayContactosBirthDay: Contacto[] = [];
 
-    let arrayContactosBirthDay: Contacto[] = [];
+      arrayContactosBirthDay = arrayContactos.filter(
+        (contacto: Contacto) => {
+          let cumpleanosArr = contacto.cumpleanos.split("-");
+          let date = new Date();
+          let month = (date.getMonth()+1);
+          let day = date.getDate();
 
-    arrayContactosBirthDay = arrayContactos.filter(
-      (contacto: Contacto) => {
-        let cumpleanosArr = contacto.cumpleanos.split("-");
-        let date = new Date();
-        let month = (date.getMonth()+1);
-        let day = date.getDate();
+        if( parseInt(cumpleanosArr[1], 10) ==  month && parseInt(cumpleanosArr[2], 10) == day){
+          return contacto;
+        }
 
-       if( parseInt(cumpleanosArr[1], 10) ==  month && parseInt(cumpleanosArr[2], 10) == day){
-         return contacto;
-       }
+        }
+      )
 
-      }
-     )
-
-     this.cantBirthDay = arrayContactosBirthDay.length;
+      this.cantBirthDay = arrayContactosBirthDay.length;
+    } else {
+      this.cantBirthDay = 0;
+      return;
+    }
   }
 
   ngOnInit(): void {
