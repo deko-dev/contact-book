30e79c7db4b436f6ad873e49af2aad3d910f9bb9
Deko-Dev <deiser@gmail.com>
2021-01-03 08:31:18 -0500

    Agregando Update Service al app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UpdatePwaService } from './services/update-pwa.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contact-book';
+
+  constructor(
+    private updateService: UpdatePwaService
+  ) {
+
+  }
 }
